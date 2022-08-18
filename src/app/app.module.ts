0ffaaf7e37fc054419a1bf0163e520b075e4a757
Yavuzhan Çaşkurlu <yavuzhancaskurlu@gmail.com>
2022-08-19 02:08:53 +0300

    Some bootstrap implementations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,13 @@ import { AuthService } from './services/auth.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { ListingAreaComponent } from './components/listing-area/listing-area.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SidebarComponent,
     ListingAreaComponent
   ],
   imports: [
