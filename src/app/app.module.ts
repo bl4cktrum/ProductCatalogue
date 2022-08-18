9cda5feaea8a8b0cf7c733b6c62d8b6c2cf53018
Yavuzhan Çaşkurlu <yavuzhancaskurlu@gmail.com>
2022-08-18 22:30:52 +0300

    Bootstrap was installed & configurated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ListingAreaComponent } from './components/listing-area/listing-area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    ListingAreaComponent
   ],
   imports: [
     BrowserModule,
