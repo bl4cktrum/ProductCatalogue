ce40fe87c4b1f765299e04d7aee4cf488d26c8d3
Yavuzhan Çaşkurlu <yavuzhancaskurlu@gmail.com>
2022-08-20 00:10:33 +0300

    updated api url. Installed json-server locally & simulated api.

--- src/app/components/listing-area/listing-area.component.ts
+++ src/app/components/listing-area/listing-area.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Product } from 'src/app/models/product';
 import { lastValueFrom, tap } from 'rxjs';
 
@@ -12,8 +12,8 @@ import { lastValueFrom, tap } from 'rxjs';
 export class ListingAreaComponent implements OnInit {
   productList: Product[] = [];
   mString: string='first';
-  url: string='https://assignment-api.piton.com.tr/api/v1/product/all';
-  accessToken: string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3QzMjFAdG1haWwuY29tIiwiaWF0IjoxNjYwNzU5MTI1LCJleHAiOjE2ODY2NzkxMjV9.lem0XHzqH5vOlryn1VimVTll2piEeU43yqQMZuteFLM";
+  apiUrl: string= "http://localhost:3000/";
+
   
 
   constructor(private http: HttpClient) { 
@@ -21,17 +21,12 @@ export class ListingAreaComponent implements OnInit {
 
   ngOnInit(): void {
     console.log("IM HERE");
-    // let headers = new HttpHeaders().set('access-token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3QzMjFAdG1haWwuY29tIiwiaWF0IjoxNjYwNzU5MTI1LCJleHAiOjE2ODY2NzkxMjV9.lem0XHzqH5vOlryn1VimVTll2piEeU43yqQMZuteFLM');
-    // this.http.get("https://assignment-api.piton.com.tr/api/v1/product/all",{headers});
     this.fetchData();
     console.log(this.mString);
   }
 
   private async fetchData(){
-    let headers = new HttpHeaders()
-    .set('Content-type','application/json')
-    .set('access-token',this.accessToken);
-    const data = await lastValueFrom(this.http.get(this.url,{headers}));
-    console.log(JSON.stringify(data));
+    const data = await lastValueFrom(this.http.get(this.apiUrl+"products"));
+    
   }
 }
